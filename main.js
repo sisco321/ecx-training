73944e22a828d68e6f7085afbe7cd2bb1db3000c
sisco321 <80267760+sisco321@users.noreply.github.com>
2021-03-08 18:01:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Paste the code from Firebase
 var config = {
     apiKey: "",
-    authDomain: "https://ecx-training-2c877.web.app/",
-    databaseURL: "https://ecx-training-2c877-default-rtdb.firebaseio.com/",
-    projectId: "ecx-training-2c877",
+    authDomain: "https://ecx-training-d34f5.web.app/",
+    databaseURL: "https://ecx-training-d34f5-default-rtdb.firebaseio.com/",
+    projectId: "ecx-training-d34f5",
     storageBucket: "",
     messagingSenderId: "",
-    appId: "1:239653999826:web:cff7327da3aa708035c90b"
+    appId: ""
 };
 firebase.initializeApp(config);
 
